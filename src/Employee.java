e8fa89f473cde6cb398ad54cc7112ef54ca51132
Andrew <blizzard383@ymail.com>
2017-03-19 16:37:29 -0500

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,11 +1,13 @@
-
-
 public class Employee {
 	String start;	
 	String first;
 	String last;
 	String dep;
 	String num;
+	String gender = null;
+	String title = null;
+	String[] Genders = {"male","female","other"};
+	String[] Titles = {"Mr.", "Ms.", "Mrs.", "Dr.","Col.","Prof."};
 	public Employee(String vars)
 	{
 		start = vars;
@@ -15,19 +17,33 @@ public class Employee {
 		dep = splitted[2];
 		num = splitted[3];
 	}
-	public Employee(String a,String b,String c,String d)
+	public Employee(String a,String b,String c,String d, boolean[] e, boolean[] f)
 	{
 		
 		first = a;
 		last = b;
 		dep = c;
 		num = d;
+		
+		for(int i = 0; i < e.length; i++)
+		{
+			if(e[i] == true)
+			{
+				gender = Genders[i];
+			}
+		}
+		for(int i = 0; i < f.length; i++)
+		{
+			if(f[i] == true)
+			{
+				title = Titles[i];
+			}
+		}
 	}
 	
 	public void print()
 	{
 		System.out.println(last + ", " + first + ", " + num + ", " + dep);
 	}
-	
 	
 }
